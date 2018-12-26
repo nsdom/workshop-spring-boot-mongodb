5a04b5231f3ecd43839f9f00b3c31cfeb19c9712
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-26 23:01:53 +0000

    Instantiation data base operation

---
+++ src/main/java/com/example/nsdom/workshopmongo/config/Instatiation.java
@@ -0,0 +1,30 @@
+package com.example.nsdom.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.nsdom.workshopmongo.domain.User;
+import com.example.nsdom.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instatiation implements CommandLineRunner {
+
+	
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
