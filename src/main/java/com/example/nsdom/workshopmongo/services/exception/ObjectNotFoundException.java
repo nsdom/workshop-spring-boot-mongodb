cdeccf9672c43d7357ab2d51b999afbfeadadec7
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-27 10:59:39 +0000

    Getting a user by id

--- src/main/java/com/example/nsdom/workshopmongo/services/exception/ObjectNotFoundException.java
+++ src/main/java/com/example/nsdom/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -1,7 +1,5 @@
 package com.example.nsdom.workshopmongo.services.exception;
 
-import javax.validation.valueextraction.ExtractedValue;
-
 public class ObjectNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
