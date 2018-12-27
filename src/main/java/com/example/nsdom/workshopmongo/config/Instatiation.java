5003048255a546d7fa3f0853c33a810248147d6f
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-27 13:54:36 +0000

    Adding comments to posts

--- src/main/java/com/example/nsdom/workshopmongo/config/Instatiation.java
+++ src/main/java/com/example/nsdom/workshopmongo/config/Instatiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.nsdom.workshopmongo.domain.Post;
 import com.example.nsdom.workshopmongo.domain.User;
 import com.example.nsdom.workshopmongo.dto.AuthorDTO;
+import com.example.nsdom.workshopmongo.dto.CommentDTO;
 import com.example.nsdom.workshopmongo.repository.PostRepository;
 import com.example.nsdom.workshopmongo.repository.UserRepository;
 
@@ -42,8 +43,14 @@ public class Instatiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Parti em viagem", "Vou viajar para Paris. Abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Hoje acordei feliz!", new AuthorDTO(maria));
-
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll((Arrays.asList(post1, post2)));
--- src/main/java/com/example/nsdom/workshopmongo/domain/Post.java
+++ src/main/java/com/example/nsdom/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.example.nsdom.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.nsdom.workshopmongo.dto.AuthorDTO;
+import com.example.nsdom.workshopmongo.dto.CommentDTO;
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -69,7 +74,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
 
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -96,6 +108,7 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 
 
 	
