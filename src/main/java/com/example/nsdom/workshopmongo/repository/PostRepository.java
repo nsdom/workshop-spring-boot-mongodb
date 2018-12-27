af8b683f4309d4b49e7b9234f51df29cd6549a36
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-27 12:53:48 +0000

    Created entity post user related

---
+++ src/main/java/com/example/nsdom/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.nsdom.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.nsdom.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
